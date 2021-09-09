bafc93467ca3df434ca39e7838bfb95e22e31392
kirin-offcial <zhuyongqi9@gmail.com>
2021-09-09 16:57:36 +0800

    add insert to table binary_task module

---
+++ insert/insertBinaryTask.js
@@ -0,0 +1,82 @@
+const { getUUID} = require('../../insert_taskdb/getParams');
+const faker = require('faker');
+const uuidParse = require('uuid-parse');
+const BinaryTask = require('../model/binaryTarget');
+
+const generateFakeData =  function (type) {
+  switch (type) {
+    case 1:
+      return faker.random.words() + faker.random.alphaNumeric();
+
+    case 2:
+      return faker.internet.email();
+
+    case 3:
+      return faker.system.fileName();
+
+    case 4:
+      return faker.system.filePath();
+
+    case 5:
+      return faker.internet.ip();
+
+    case 6:
+      return faker.internet.domainName();
+
+    case 7:
+      return faker.internet.url();
+
+    case 8:
+      return faker.internet.userName();
+  }
+}
+
+const insertBinaryTask = async function (index) {
+  for (let i = index; i < index + 1; i++){
+    console.log(i);
+    
+    let taskInfo = {};
+
+    taskInfo.customerId = uuidParse.parse(await getUUID(i));
+    console.log(taskInfo.customerId);
+    taskInfo.id = String(i+1).padStart(8, 0)
+    taskInfo.targetType = (i % 8) + 1;
+    taskInfo.target = '{}';
+    taskInfo.displayTarget = '{}';
+    taskInfo.details = '{}';
+    taskInfo.taskStatus = (i % 4) + 1;
+    taskInfo.aciton = i % 20;
+    taskInfo.retryCount = Math.floor(Math.random() * 10);
+    taskInfo.createdTime = new Date().toLocaleString();
+
+    let executedTime = new Date();
+    executedTime.setMonth(9);
+    taskInfo.executedTime = executedTime.toLocaleString();
+
+    let finishedTime = new Date();
+    finishedTime.setMonth(10);
+    taskInfo.finishedTime = finishedTime.toLocaleString();
+
+    taskInfo.errorCode = Math.floor(Math.random() * 10);
+    taskInfo.taskCount = Math.floor(Math.random() * 10);
+    taskInfo.accountName = 'richard_james';
+    taskInfo.sourceName = Math.floor(Math.random() * 10);
+    taskInfo.sourceId = String(i+1).padStart(36, 0)
+    taskInfo.description = '{}';
+
+    BinaryTask.create(taskInfo);
+  }
+}
+
+insertBinaryTask(2);
+
+const runInsertBinaryTask = async function () {
+  let insertPromiseArr = []
+  for (let i = 0; i<500; i++){
+    insertPromiseArr.push(updateBinaryTask(1000 * i));
+  }
+  await Promise.all(insertPromiseArr);
+  console.log(`it cost ${end - start} millionseconds`);
+}
+
+// runInsertBinaryTask();
